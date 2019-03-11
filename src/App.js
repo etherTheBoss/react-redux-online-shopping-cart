e3a345f5dc1d5232426a70199599ea320d97840a
Habibur Rahman <hrekns@gmail.com>
2019-03-12 04:28:02 +0600

    links added for router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import Router from './Router';
+import {NavLink} from 'react-router-dom';
+
+
+const Navigation =  (props) =>
+<nav>
+  <ul>
+    <li><NavLink to ='/'>Home</NavLink></li>
+    <li><NavLink to ='/cart'>Cart</NavLink></li>
+  </ul>
+</nav>
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Shopping Cart</h1>
-
+        <Navigation />
         <Router />
       </div>
     );
